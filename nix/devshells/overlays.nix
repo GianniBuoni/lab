8733b549286801928f9080ed16c22730fa5fdf7f
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-01-26 20:48:40 -0500

    chore: add sops creation rules for cluster contexts
    - add path to age keys to dev shells

--- nix/devshells/overlays.nix
+++ nix/devshells/overlays.nix
@@ -2,15 +2,28 @@ let
   mkEnv = name: value: {inherit name value;};
   mkTls = name: extension: cluster: {
     inherit name;
-    value = "${baseTlsPath}/${cluster}/tls.${extension}";
+    value = "${baseSecretsPath}/${cluster}/tls.${extension}";
   };
-  baseTlsPath = "/run/secrets";
+  mkAge = cluster: {
+    name = "SOPS_AGE_KEY_FILE";
+    value = "${baseSecretsPath}/${cluster}/age.agekey";
+  };
+  baseSecretsPath = "/run/secrets";
 in {
   flake.aspects.devshells.dev = let
     clusterContext = "dev";
+    baseIP = "10.0.0";
+    nodeIP = ["179" "153" "193"];
+    nodes =
+      builtins.concatStringsSep " "
+      (builtins.map (ip: "${baseIP}.${ip}") nodeIP);
   in {
     env = [
+      (mkAge "${clusterContext}")
       (mkEnv "CLUSTER_BRANCH" "${clusterContext}")
+      (mkEnv "TALOS_CLUSTER" "staging-gary")
+      (mkEnv "TALOS_IPS" "${nodes}")
+      (mkEnv "TALOS_NODE" "staging")
       (mkTls "TLS_CRT_FILE" "crt" "${clusterContext}")
       (mkTls "TLS_KEY_FILE" "key" "${clusterContext}")
     ];
@@ -19,6 +32,7 @@ in {
     clusterContext = "prod";
   in {
     env = [
+      (mkAge "${clusterContext}")
       (mkEnv "CLUSTER_BRANCH" "${clusterContext}")
       (mkTls "TLS_CRT_FILE" "crt" "${clusterContext}")
       (mkTls "TLS_KEY_FILE" "key" "${clusterContext}")
